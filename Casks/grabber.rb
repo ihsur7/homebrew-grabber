fc31af2dc2ed44210fc3bfd327d8495e7af86cd4
Rushabh Patel <ihsur7@gmail.com>
2026-04-10 18:47:28 +0200

    Create Cask for Grabber version 1.0.0
    Add Cask for Grabber utility with version 1.0.0

---
+++ Casks/grabber.rb
@@ -0,0 +1,23 @@
+cask "grabber" do
+  version "1.0.0"
+  sha256 "11652e07fbd530f9900fecdc108bb87bbf7624e99dc3b8ab565f96ded001f90d"
+
+  url "https://github.com/ihsur7/grabber/releases/download/v#{version}/grabber-#{version}.zip"
+  name "Grabber"
+  desc "Menu bar utility for moving windows by holding a modifier key"
+  homepage "https://github.com/ihsur7/grabber"
+
+  depends_on macos: ">= :sonoma"
+
+  app "grabber.app", target: "Grabber.app"
+
+  livecheck do
+    url :homepage
+    strategy :github_latest
+  end
+
+  zap trash: [
+    "~/Library/Preferences/com.ihsur.grabber.plist",
+    "~/Library/Saved Application State/com.ihsur.grabber.savedState",
+  ]
+end
