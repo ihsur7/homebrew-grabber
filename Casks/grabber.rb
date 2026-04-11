d139d9d0fe3f15d5d9b4c9a4a28c9e61777b7da3
Xcode Cloud <xcode-cloud@users.noreply.github.com>
2026-04-11 11:51:08 -0700

    Update grabber cask to v1.0.27

--- Casks/grabber.rb
+++ Casks/grabber.rb
@@ -1,6 +1,6 @@
 cask "grabber" do
-  version "1.0.26"
-  sha256 "1367406833f66f60e8abd10a26da62ddf13c4f6fa421be8052c67c67cbe43c55"
+  version "1.0.27"
+  sha256 "e81ab05f28f6e906ebd2d21c1927d52721bfad7afc33708acdf12a6ba6624be4"
 
   url "https://github.com/ihsur7/grabber/releases/download/v#{version}/grabber-#{version}.zip"
   name "Grabber"
